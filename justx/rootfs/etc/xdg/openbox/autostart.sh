c2baccb9f83fa6048077018799797bb93fd9a9d3
Christophe Lincoln <pankso@slitaz.org>
2010-02-02 22:26:52 +0100

    Keep 4 flavors + loram + 3in1 and add tmp addfiles for core

---
+++ justx/rootfs/etc/xdg/openbox/autostart.sh
@@ -0,0 +1,35 @@
+# This shell script is run before Openbox launches.
+# Environment variables set here are passed to the Openbox session.
+
+# DBUS message bus (automount removable devices)
+#dbus-launch --exit-with-session &
+
+# Handle Hal events with Ivman (notification with desktopbox).
+#ivman &
+
+# Start PCmanFM as daemon for Wallpaper and desktop icons.
+#pcmanfm -d &
+
+# Start the Freedesktop panel standard menu.
+#lxpanel &
+
+# Start Parcellite clipboard manager.
+#parcellite &
+
+# Launch Xpad desktop notes utility.
+#xpad &
+
+# Desktop effects composer.
+#xcompmgr -c -r 10 &
+
+# Set a background image using hsetroot (depends on imlib2).
+hsetroot -fill /usr/share/images/slitaz-background.jpg &
+
+# Start Tint2 desktop panel
+tint2 &
+
+# Background color with xsetroot.
+#xsetroot -solid "#222222" &
+
+# Wbar icons panel.
+#(sleep 2 && wbar -above-desk -pos top center -jumpf 0 -zoomf 1.5 -bpress -balfa 0) &
